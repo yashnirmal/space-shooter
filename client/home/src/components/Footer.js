256e1b3f8d90b258988e186dbae88cdf562fbdd7
yash nirmal <yash15nirmal@gmail.com>
2023-02-06 19:20:07 +0530

    fixed the collision detection bug and made the footer stick to bottom

--- client/home/src/components/Footer.js
+++ client/home/src/components/Footer.js
@@ -1,11 +1,11 @@
 import "./Footer.css";
 import {Link} from "react-router-dom";
 
+
 export default function Footer(){
 	return (
 		<div className="footer-cont">
 			<div>
-
 				<div>
 					<Link className="logo" to="/">
 						SpaceShooter
